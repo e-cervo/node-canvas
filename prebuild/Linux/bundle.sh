b2d10812d6e0b82eef3126d0633d15cfa78433c3
Philippe Plantier <philippe.plantier@e-cervo.com>
2023-09-08 15:36:48 +0200

    use debian archive on stretch

--- prebuild/Linux/bundle.sh
+++ prebuild/Linux/bundle.sh
@@ -2,7 +2,12 @@ set -ex
 
 copies=$(lddtree.sh -l build/Release/canvas.node | sed -r -e '/^\/lib/d' -e '/canvas.node$/d');
 
-apt update
+# Fix the apt repository if the debian version is stretch
+if [ "$(cat /etc/debian_version | cut -f1 -d.)" = "9" ]; then
+  echo "deb http://archive.debian.org/debian stretch main contrib non-free" > /etc/apt/sources.list
+  apt update
+fi
+
 apt install -y patchelf
 
 for so in $copies; do
